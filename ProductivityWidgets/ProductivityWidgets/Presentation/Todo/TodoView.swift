6d2755da0b060e86a7327df89a52c894f762abab
Eirado <111468283+Eirado@users.noreply.github.com>
2025-04-30 14:27:38 -0300

    ,

--- ProductivityWidgets/ProductivityWidgets/Presentation/Todo/TodoView.swift
+++ ProductivityWidgets/ProductivityWidgets/Presentation/Todo/TodoView.swift
@@ -10,34 +10,33 @@ import SwiftData
 struct TodoView: View, SizedViewProtocol {
     var size: CGSize
     var safeArea: EdgeInsets
-
+    
     @State private var viewModel: TodoViewModel
     
     @State private var focusedTodoID: UUID? = nil
     @FocusState private var focusedField: UUID?
-
+    
     @Query(
-            sort: [
-                SortDescriptor(\Todo.isCompleted, order: .forward),
-                SortDescriptor(\Todo.lastModified, order: .forward)
-            ]
-        ) private var todos: [Todo]
-
+        sort: [
+            SortDescriptor(\Todo.isCompleted, order: .forward),
+            SortDescriptor(\Todo.lastModified, order: .forward)
+        ]
+    ) private var todos: [Todo]
+    
     init(size: CGSize, safeArea: EdgeInsets, viewModel: TodoViewModel) {
         self.size = size
         self.safeArea = safeArea
         self.viewModel = viewModel
     }
-
+    
     var body: some View {
         NavigationStack {
             List {
-                withAnimation(.snappy) {
-                    ForEach(todos) { todo in
-                        TodoRowView(todo: todo)
-                    }
+                ForEach(todos) { todo in
+                    TodoRowView(todo: todo)
                 }
             }
+            .animation(.snappy, value: todos)
             .navigationTitle("Todo List")
             .toolbar {
                 ToolbarItem(placement: .bottomBar) {
@@ -53,7 +52,9 @@ struct TodoView: View, SizedViewProtocol {
                 }
             }
         }
-    }}
+    }
+}
+
 #Preview {
     TodoViewFactory.makeTodoView(
         size: DevicePreview.iPhone16Pro.size,
